a6b4e4b5acd7cb105ad096d0a475ff7951a1f9ce
TunedayCodes <92256566+TunedayCodes@users.noreply.github.com>
2022-11-25 08:53:22 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+This is an app that's built to take in inputs in numbers from user with the goal of guessing the right digits programmed in the app.
+
+We have an heading stating the purpose of the game to the user then a paragraph that explains further what the app is all about and the neceesary instructions.
+
+Then an input is added to take in users guess and a button to check if the guess is correct and it return result of the guess with the help of Javascript.
+
+I was able to write a code to generate a random number between 1 and 100 which serves as the picked number to be guessed correctly and its changes a the start of every new game.
+
+I provided the player a way to guess and input what digit they want to if its correct, a congratulatory message will pop and if otherwise a message will tell the player they are wrong.
---
+++ guessdigit.js
@@ -0,0 +1,66 @@
+//EVENT LISTENER FOR CLICK EVENT OF BUTTON
+document.getElementById("check").addEventListener("click", validateGuess);
+document.getElementById("start").addEventListener("click", newGame);
+
+//DECLARE VARIABLES
+let pickedNumber = 0;
+const maxTries = 10;
+let count = 0;
+let gameOver = false;
+
+//CALL FUNCTION TO START THE GAME
+newGame();
+
+//CREATE FUNCTION TO VALIDATE USER'S GUESSESS
+function validateGuess(){
+  let message = "";
+
+  //GET THE USER'S GUESS FROM THE TEXT BOX
+  const userGuess = parseInt(document.getElementById("guess").value);
+  console.log("The user guess  is " + userGuess);
+  //console.log("type is " + typeof(userGuess));
+
+  //COMPARE USER'S GUESS TO PICKED NUMBER
+  if(pickedNumber == userGuess){
+    message = "Nice one! you guessed right"
+    gameOver = true;
+    
+//RE-ENABLE THE START NEW GAME BUTTON IF THE ABOVE CONDITION IS MET ALREADY
+document.getElementById("start").disabled = false;
+    
+  } 
+  else if(userGuess<pickedNumber){
+    message = "uhmmm... Too low"
+  } 
+  else{
+    message = "Oops! Too high"
+  }
+
+//INCREMENT COUNTER
+  count++; //THIS IS SAME AS count = count + 1; or count += 1;
+  console.log(message);
+
+  //THIS CHECKS IF THE USER IS OUT OF TRIALS
+  if (count == maxTries && gameOver == false){
+    message = "Sorry, you are out of trials. The number was " + pickedNumber;   
+//RE-ENABLE THE START NEW GAME BUTTON IF THE ABOVE CONDITION IS MET ALREADY
+document.getElementById("start").disabled = false;
+  }
+  
+//DISPLAY THE OUTCOME OF USER'S GUESSESS 
+document.getElementById("msg").textContent=message;
+} //THE CHECKGUESS ENDS HERE
+
+//CREATING A FUNCTION TO RE-ENABLE THE START NEW GAME BUTTON TO REFRESH THE GAME FOR A NEW USER
+function newGame(){
+  //console.log("new game");
+document.getElementById("start").disabled = true;
+  
+//GENERATE A NEW RANDOM NUMBER TO GUESS BETWEEN 1 TO 100
+pickedNumber = 1 + Math.floor(Math.random()*100);
+//console.log("pickedNumber is " + pickedNumber);
+
+//RESET VARIABLES  
+count = 0;
+gameOver = false;
+}
